67d9beb7bd4aed56ffa16b387c53e17c5135d797
Eyck Jentzsch <eyck@minres.com>
2019-06-11 16:49:37 +0000

    reorganized layout to only contain risc-v stuff

--- build.sh
+++
@@ -1,6 +0,0 @@
-mkdir -f build/Release
-cd build/Release
-cmake ../.. -DCMAKE_BUILD_TYPE=RelWithDebInfo && \
-	cmake --build . && \
-	bin/riscv --reset=0x20400000 --verbose=4 $HOME/eclipse-workspace/RiscV-dhrystone/dhrystone
-
